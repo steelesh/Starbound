48725a43f045e6e1a48faf47758c55d510c87bbd
Steele <shrevesa@mail.uc.edu>
2023-04-20 21:42:49 -0400

    Edit platform script

--- Assets/Scripts/White_Platform.cs
+++ Assets/Scripts/White_Platform.cs
@@ -9,15 +9,16 @@ public class White_Platform : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         if (collision.relativeVelocity.y <= 0f)
         {
+            Main_Player player = collision.collider.GetComponent<Main_Player>();
             Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
                 Vector2 velocity = rb.velocity;
                 velocity.y = jumpForce;
                 rb.velocity = velocity;
+                player.animator.SetBool("isJumping", true);
             }
         }
     }
